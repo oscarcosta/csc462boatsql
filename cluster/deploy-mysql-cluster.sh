aaffaf81ece4cca561c017cfddf2027eee050d2a
Oscar Costa <oscarcosta@gmail.com>
2019-08-02 12:34:20 -0700

    Docker image for web-app

--- cluster/deploy-mysql-cluster.sh
+++ cluster/deploy-mysql-cluster.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# build the docker image
+# build the docker images
 docker build --no-cache -t mysql-cluster ./mysql-cluster/
+
+cp -r ../web web-app/web
+docker build --no-cache -t web-app ./web-app/
 
 # create the docker network
 #docker network create --attachable --driver overlay cluster
@@ -13,6 +16,9 @@ docker-compose up
 # Commands to verify the containers
 # docker ps
 # docker inspect cluster_mysql1_1
+
+# Commands to verify cluster
+# docker exec -it cluster_mysql1_1 ndb_mgm -e "show"
 
 # Import database
 # docker exec -it cluster_mysql1_1 mysql -u root -e "$(cat ../boat.sql)"

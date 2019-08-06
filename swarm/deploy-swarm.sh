b883c220ec1ecc29121e1efa3a70adf563ddabc4
Oscar Costa <oscarcosta@gmail.com>
2019-08-05 20:35:26 -0700

    docker-compose to gatekeeper

--- swarm/deploy-swarm.sh
+++ swarm/deploy-swarm.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
-# build the docker images
-#cp ../boat.sql mysql-cluster/
-#docker build --no-cache -t mysql-cluster ./mysql-cluster/
-
-#cp -r ../web web-app/web
-#docker build --no-cache -t web-app ./web-app/
-
 # create the docker network
 docker network create --driver overlay cluster_net
 
 # deploy all
+#docker stack deploy -c docker-compose-gatekeeper.yml boatapp
 docker stack deploy -c docker-compose.yml boatapp
 
 # undeploy

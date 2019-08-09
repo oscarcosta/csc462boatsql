56b38154ca47b93c2ef96d481bc8ffc8f4e55494
Oscar Costa <oscarcosta@gmail.com>
2019-08-09 14:15:55 -0700

    update publish script

--- swarm/publish-images.sh
+++ swarm/publish-images.sh
@@ -3,9 +3,9 @@
 # Build, Tag and Push dock images to docker hub repositores
 
 # oscarcosta/boat-web-app
+rm -rf web-app/web/
 cp -r ../web web-app/web
 docker build --no-cache -t web-app ./web-app/
 
-docker tag web-app:latest oscarcosta/boat-web-app:latest
+docker tag web-app oscarcosta/boat-web-app:latest
 docker push oscarcosta/boat-web-app:latest
-

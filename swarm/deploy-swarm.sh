479e1ead1372d344f3a08b580470fa0a2962b0f8
Oscar Costa <oscarcosta@gmail.com>
2019-08-09 13:55:04 -0700

    update script

--- swarm/deploy-swarm.sh
+++ swarm/deploy-swarm.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+# undeploy stack
+docker stack rm boatapp
+
+# download images
+docker pull dockersamples/visualizer
+docker pull oscarcosta/boat-web-app
+
 # create the docker network
 docker network create --driver overlay --attachable cluster
 
 # deploy all
 docker stack deploy -c docker-compose.yml boatapp
-
-# undeploy
-# docker stack rm boatapp

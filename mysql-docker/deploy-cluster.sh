0b885e8626ee40985cea93296c5fb6e1f6a22c46
Oscar Costa <oscarcosta@gmail.com>
2019-08-08 18:24:22 -0700

    Local docker cluster

--- mysql-docker/deploy-cluster.sh
+++ mysql-docker/deploy-cluster.sh
@@ -4,18 +4,18 @@
 PASSWORD='MyPass!2'
 CLUSTER='cluster'
 
-
 # download the MySQL Server Docker Image
 echo "# Downloading the docker image..."
 
 docker pull mysql/mysql-cluster
 
+cp -r ../web web-app/web
+docker build --no-cache -t web-app ./web-app/
 
 # create the internal docker network
 echo "# Creating docker networkd..."
 
 docker network create $CLUSTER --subnet=192.168.0.0/16
-
 
 # start the management nodes
 echo "# Starting management nodes..."
@@ -42,31 +42,40 @@ docker run -d --net=$CLUSTER --name=$SERVER --ip=192.168.0.20 -e MYSQL_ROOT_PASS
 #docker run -d --net=$CLUSTER --name=$SERVER --ip=192.168.0.10 -e MYSQL_ROOT_PASSWORD=$PASSWORD mysql/mysql-cluster mysqld
 #docker run -d --net=$CLUSTER --name=$SERVER --ip=192.168.0.10 -e MYSQL_RANDOM_ROOT_PASSWORD=true mysql/mysql-cluster mysqld
 
-
-# wait the server start
-echo "# Please, hold on until the system initializes..."
-sleep 30
-
+# wait the cluster to start
+echo "# Please, hold on until the cluster initializes..."
+sleep 10
 
 # change the mysql root password
 #temp_pass=$(docker logs $SERVER 2>&1 | grep 'PASSWORD' | awk '{print $5}')
 #docker exec -d $SERVER mysql --user=root --password=$temp_pass -e $(echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$PASSWORD';")
 
+# Configure the user credentials
 echo "# Configuring server credentials..."
-# grant remote access for root (TODO why isn't working?)
+
 docker exec -d $SERVER mysql --user=root --password=$PASSWORD -e "CREATE USER 'root'@'%' IDENTIFIED BY '$PASSWORD';"
 docker exec -d $SERVER mysql --user=root --password=$PASSWORD -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%';"
 docker exec -d $SERVER mysql --user=root --password=$PASSWORD -e "FLUSH PRIVILEGES;"
 #SELECT host, user FROM mysql.user;
 
+# Create the database
+echo "# Create BoatDB..."
+
+docker exec -d $SERVER mysql --user=root --password=$PASSWORD -e "$(cat ../boat.sql)"
+
+# start the web application
+echo " Starting web application..."
+
+docker run -d --net=$CLUSTER --name='web' --ip=192.168.0.30 -p 3000:3000 -e MYSQL_host=192.168.0.20 -e MYSQL_database=boatdb -e MYSQL_user=root -e MYSQL_password=$PASSWORD -e CSV_filename=data.csv web-app
+
 echo "# Finalizing..."
 # check cluster status
-docker run -it --net=$CLUSTER --volume=$(pwd)/mysql-cluster.cnf:/etc/mysql-cluster.cnf mysql/mysql-cluster ndb_mgm -e "show"
 
+docker run -it --net=$CLUSTER --volume=$(pwd)/mysql-cluster.cnf:/etc/mysql-cluster.cnf mysql/mysql-cluster ndb_mgm -e "show"
+docker ps
 
 # Connecting to MySQL Server from within the Container
 #docker exec -it mysql1 mysql -uroot -p
-
 
 # Container Shell Access
 #docker exec -it mysql1 bash 

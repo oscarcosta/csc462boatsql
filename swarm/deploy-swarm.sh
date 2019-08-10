31c4bd4cd83e2a5e53c9d0ea25ec6033dda9e4fb
Oscar Costa <oscarcosta@gmail.com>
2019-08-10 10:33:10 -0700

    Deploying

--- swarm/deploy-swarm.sh
+++ swarm/deploy-swarm.sh
@@ -10,7 +10,7 @@ docker pull dockersamples/visualizer
 rm -rf web-app/web/
 cp -r ../web web-app/web
 docker build --no-cache -t web-app ./web-app/
-docker pull web-app
+#docker pull web-app
 
 # create the docker network
 docker network create --driver overlay --attachable cluster
